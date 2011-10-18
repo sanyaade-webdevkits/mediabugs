7529bcf27aeecc5ea49d5e6dabaed9ddf44f5ef5
Sebastian Motraghi <sebastian.m@gmail.com>
2011-10-18 16:32:16 -0500

    a rudimentary admin tool.

--- pods/bugs_spv_admin/handler.php
+++ pods/bugs_spv_admin/handler.php
@@ -5,6 +5,42 @@ $POD = new PeoplePod(array(
 	'lockdown' => 'adminUser'
 ));
 
+// auto create an admin record if one does not already exist.
+$admin_records = $POD->getContents(array(
+	'type' => 'admin_record',
+	'headline' => 'admin record'
+));
+
+if ($admin_records->count() == 0) {
+	// need to create an admin record.
+	$admin_record = $POD->getContent();
+	$admin_record->set('type', 'admin_record');
+	$admin_record->set('headline', 'admin record');
+	
+	$admin_record->save();
+} else {
+	$admin_record = $admin_records->getNext();
+}
+
+if (!empty($_POST)) {
+	if (!empty($_FILES)) {
+		$admin_record->addFile('publication_img', $_FILES['publication_img']);
+		$admin_record->files()->fill();
+	}
+
+	if (isset($_POST['publication_name'])) {
+		$admin_record->publication_name = $_POST['publication_name'];
+	}
+	
+	if (isset($_POST['publication_url'])) {
+		$admin_record->publication_url = $_POST['publication_url'];
+	}
+	$admin_record->save();
+
+}
+
+$POD->adminRecord = $admin_record;
+
 $POD->header('Admin Tool');
 
 $POD->output('admintool');
--- themes/mediabugs/admintool.php
+++ themes/mediabugs/admintool.php
@@ -1,5 +1,39 @@
+<?php
+if ($logo = $POD->adminRecord->files()->contains('publication_img')) {
+	// we have a logo.
+} else {
+	$logo = false;
+}
+
+
+?>
 <div class='grid'>
 	<div class='column_5'>
-	Admin Tool
+		<form action='<?php $POD->siteRoot()?>/spvadmin' method='POST'
+				enctype='multipart/form-data'>
+			<p class='input'>
+				<label for='publication_name'>Publication Name</label>
+				<input name="publication_name" type='text' 
+					value='<?php echo $POD->adminRecord->publication_name ?>'>
+			</p>
+			<p class='input'>
+				<label for='publication_url'>Publication URL</label>
+				<input name="publication_url" type='text'
+					value='<?php echo $POD->adminRecord->publication_url ?>'>
+			</p>
+			<p class='input'>
+				<label for='publication_img'>Publication Image</label>
+				<?php if ($logo): ?>
+					<img src='<?php echo $logo->src(100) ?>'>
+				<?php else: ?>
+					<span>(no logo has been uploaded yet)</span>
+				<?php endif ?>
+				<input type='file' name='publication_img'>
+			</p>
+			
+			<p class='input'>
+				<input type='submit' value='Update Publication'>
+			</p>
+		</form>
 	</div>
 </div>
