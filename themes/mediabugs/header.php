7a9ac5a0fc1360c84291f7d679d2c545d27f8b50
Sebastian Motraghi <sebastian.m@gmail.com>
2011-10-19 15:22:48 -0500

    Pagelinks across the top bar.

--- themes/mediabugs/header.php
+++ themes/mediabugs/header.php
@@ -16,6 +16,8 @@
 /**********************************************/
 
 $adminRecord = $POD->getContents(array('type'=>'admin_record'))->getNext();
+$topPages = $POD->getContents(array('type'=>'page'), 'date ASC', 3);
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -149,6 +151,13 @@ $adminRecord = $POD->getContents(array('type'=>'admin_record'))->getNext();
 							<div class="clearer"></div>
 					</div>	
 				</li>
+				<?php foreach ($topPages as $page): ?>
+					<li>
+						<a href="<?php echo $page->permalink ?>">
+							<?php echo $page->headline ?>
+						</a>
+					</li>
+				<?php endforeach ?>
 			</ul>
 			</div>
 			<div class="column_4_simple last">		
