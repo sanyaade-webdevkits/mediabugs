84dcfdfd1e359b80d7c6729889115478a3b8f7d7
Sebastian Motraghi <sebastian.m@gmail.com>
2011-10-20 16:12:36 -0500

    If an anonymous user provided a name at bug creation, display it in the bug history.

--- themes/mediabugs/content/bug.history.php
+++ themes/mediabugs/content/bug.history.php
@@ -1,3 +1,10 @@
+<?php
+$author_name = $comment->parent()->author()->nick;
+if (!empty($comment->parent()->who_name) && !$comment->parent()->author_name->adminUser) {
+	$author_name = $comment->parent()->who_name;
+}
+?>
+
 <div class="bug_history <? if ($comment->isOddItem){?>bug_odd<? } ?>">
 	<span class="history_date">
 		<?= date('M d, Y',strtotime($comment->date)); ?>
@@ -12,7 +19,7 @@
 		<img src="<? $POD->templateDir(); ?>/img/status_icons/<?= $POD->tokenize($comment->status); ?>_20.png" />
 	</span>
 	<span class="history_person">
-		<b><?php echo $comment->author()->nick ?></b>
+		<b><?php echo $author_name ?></b>
 	</span>
 	<div class="clearer"></div>
 </div>
