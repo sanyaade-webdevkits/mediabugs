a21715ee9fa88dc9cdc24c2e8177afcd29a30741
Sebastian Motraghi <sebastian.m@gmail.com>
2011-10-21 11:24:06 -0500

    Display errors on the bug form, if they are passed in through the get.

--- themes/mediabugs/content/bug.form.php
+++ themes/mediabugs/content/bug.form.php
@@ -44,6 +44,9 @@ if (!$doc->saved() || $POD->currentUser()->adminUser || (time() - strtotime($doc
 
 ?>
 
+<?php if ($_GET['msg']): ?>
+<div class='error_message'><?php echo htmlspecialchars($_GET['msg']) ?></div>
+<?php endif ?>
 <ul id="bug_tabs">
 	<? if (!$doc->saved()) { ?>
 	<li id="tab_report" class="active">
