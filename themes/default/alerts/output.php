4f9e68d78157299515664374376c35ea7122f482
Sebastian Motraghi <sebastian.m@gmail.com>
2011-10-24 14:24:15 -0500

    Remove default peoplepods theme.

--- themes/default/alerts/output.php
+++
@@ -1,4 +0,0 @@
-<div class="alert" id="alert<?= $this->id; ?>">
-	<?= $this->formatMessage(); ?>
-	<a href="#markAsRead" data-alert="<?= $this->id; ?>" class="markAsRead">x</a>
-</div>
--- themes/default/sidebars/activity_stream.php
+++
@@ -1,9 +0,0 @@
-<? if ($POD->isAuthenticated()) { ?>
-	<? $activity = $POD->currentUser()->getActivityStream(); ?>
-	<div class="sidebar padded" id="activity_stream_sidebar">
-		<h3>Recent Activity</h3>
-		<ul>
-			<? $activity->output('output'); ?>
-		</ul>
-	</div>
-<? } ?>
